ec0bf184600e5607e3d9deeac7f68edc76c8f96c
green <eastking34@gmail.com>
2026-06-29 17:38:42 +0900

    Calculator 클래스로 App.java 구현

--- src/App.java
+++ src/App.java
@@ -5,9 +5,8 @@ import java.util.ArrayList;
 public class App {
     public static void main(String[] args) {
 
-        // 반복 연산 위해 컬렉션(리스트) 사용
-        List<Double> results = new ArrayList<>();
-        int count = 0;
+        // Calculator 인스턴스
+        Calculator calculator = new Calculator();
 
         Scanner sc = new Scanner(System.in);
 
@@ -24,54 +23,24 @@ public class App {
             System.out.print("사칙연산 기호를 입력하세요((+, -, *, /): ");
             char operator = sc.next().charAt(0);
 
-            // 변수 초기화 -> 타입은 double(나눗셈 소수점 연산 위해)
-            double result = 0;
-
-            // 연산자 처리
-            switch (operator) {
-                case '+':
-                    result = num1 + num2;
-                    break;
-                case '-':
-                    result = num1 - num2;
-                    break;
-                case '*':
-                    result = num1 * num2;
-                    break;
-                case '/':
-                    // 분모 0 처리
-                    if (num2 == 0) {
-                        System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
-                        continue;
-                    }
-                    result = (double) num1 / num2;
-                    break;
-                // 사칙연산 기호에는 +, -, *, / 만 가능
-                default:
-                    System.out.println("올바른 사칙연산 기호가 아닙니다. (+, -, *, / 만 가능)");
-                    continue;
-            }
+            // Calculator의 calculate 메서드로 연산자 처리
+            double result = calculator.calculate(num1, num2, operator);
             System.out.println("결과: " + result);
-
-            // 연산 결과 리스트에 저장
-            results.add(result);
 
             // remove 입력 시 첫 번째 연산 결과 제거
             System.out.println("가장 먼저 저장된 연산 결과를 삭제하시겠습니까? (remove 입력 시 삭제)");
             String input = sc.next();
             if (input.equals("remove")) {
-                results.remove(0);
+                calculator.results.remove(0);
             }
             // inquiry 입력 시 연산 결과 반환
             System.out.println("저장된 연산결과를 조회하시겠습니까? (inquiry 입력 시 조회)");
             input = sc.next();
             if (input.equals("inquiry")) {
-                for (double i : results) {
+                for (double i : calculator.results) {
                     System.out.print(i + ", ");
                 }
             }
-
-
 
             // exit 전까지 연산 반복
             System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
